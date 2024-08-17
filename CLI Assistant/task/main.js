422885fc915b077a5b2b269869875b59562b4ed1
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-08-17 18:15:55 +0200

    Stage 2/3

--- CLI Assistant/task/main.js
+++ CLI Assistant/task/main.js
@@ -1,13 +1,53 @@
-const args = process.argv;
+const fs = require("node:fs");
+const path = require("node:path");
 
-args.forEach((arg) => {
-    if (arg.endsWith("=")) {
-        console.log("It seems you forget to specify argument!");
-    } else if (arg.startsWith("--")) {
-        let elements = arg.slice(2).split("=");
-        console.log(`${elements[0]}: ${elements[1] ? elements[1] : "true"}`);
+const args = process.argv.slice(2);
 
-    } else if (arg.startsWith("-")) {
-        console.log(`${arg.slice(1)}: true`);
+
+try {
+  args.forEach(arg => {
+    const [command, value] = arg.split('=');
+
+    switch (command) {
+      case '--exist':
+        const filePath = path.resolve(__dirname, value);
+
+        if (fs.existsSync(filePath)) {
+          console.log(`The file ${value} exists!`);
+          console.log(value);
+        } else {
+          console.log(`File ${value} doesn't exist!`);
+        }
+        break;
+
+      case '--create':
+        const dirPath = path.resolve(__dirname, value);
+        if (value.includes('/')) {
+          fs.mkdirSync(dirPath, { recursive: true });
+          console.log(`The folders ${value} were created!`);
+        } else {
+          fs.mkdirSync(dirPath, { recursive: true });
+          console.log(`The folder ${value} was created!`);
+        }
+        break;
+
+      case '--remove':
+        const fullPath = path.resolve(__dirname, value);
+        if (value.includes('/')) {
+          fs.rmdirSync(fullPath, { recursive: true });
+          console.log(`The folder ${value.split('/')[0]} in ${value.split('/')[1]} folder was deleted!`);
+        } else if (!(fs.existsSync(fullPath) && fs.statSync(fullPath).isDirectory() && fs)) {
+          console.log(`This ${value} folder doesn't exist!`);
+        } else {
+          fs.rmdirSync(fullPath, { recursive: true });
+          console.log(`The folder ${value} was deleted!`);
+        }
+        break;
+
+      default:
+        console.log('Unknown command.');
     }
-});
+  });
+} catch (error) {
+  console.error(`An error occurred: ${error.message}`);
+}
--- CLI Assistant/task/test/tests.py
+++ CLI Assistant/task/test/tests.py
@@ -1,3 +1,4 @@
+import os
 from hstest import *
 
 
@@ -5,94 +6,143 @@ class CliTest(StageTest):
     @dynamic_test()
     def test_1(self):
 
-        main = TestedProgram()
-        output = main.start("-h")
-
-        solution = "h: true"
-
-        len_output = sum(len(s) for s in output.split("\n"))
-
-        if len(output) == 0:
-            return CheckResult.wrong("It seems that your program does not print anything to the console!")
-
-        if len_output > len(solution):
-            return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
-                                     "\nYour output is:\n" + output + "You may have printed extra parameters!")
-
-        if solution not in output:
-            return CheckResult.wrong(f"Expected to see the following line in the console: \n" + solution +
-                                     "\nBut your output is:\n" + output)
-
-        return CheckResult.correct()
-
-    @dynamic_test()
-    def test_2(self):
+        with open("data.txt", "w") as file:
+            file.write("Some text of data")
 
         main = TestedProgram()
-        output = main.start("--help", "-p")
+        output = main.start("--exist=data.txt")
 
-        solution = "help: true\np: true"
+        os.remove("data.txt")
 
-        len_output = sum(len(s) for s in output.split("\n"))
-        len_solution = sum(len(s) for s in solution.split("\n"))
-
-        if len(output) == 0:
-            return CheckResult.wrong("It seems that your program does not print anything to the console!")
-
-        if len_output > len_solution:
-            return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
-                                     "\nYour output is:\n" + output + "You may have printed extra parameters!")
-
-        if solution not in output:
-            return CheckResult.wrong(f"Expected to see the following lines in the console: \n" + solution +
-                                     "\nBut your output is:\n" + output)
-
-        return CheckResult.correct()
-
-    @dynamic_test()
-    def test_3(self):
-
-        main = TestedProgram()
-        output = main.start("-l", "--create=file.txt", "--debug")
-
-        solution = "l: true\ncreate: file.txt\ndebug: true"
-
-        len_output = sum(len(s) for s in output.split("\n"))
-        len_solution = sum(len(s) for s in solution.split("\n"))
-
-        if len(output) == 0:
-            return CheckResult.wrong("It seems that your program does not print anything to the console!")
-
-        if len_output > len_solution:
-            return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
-                                     "\nYour output is:\n" + output + "You may have printed extra parameters!")
-
-        if solution not in output:
-            return CheckResult.wrong(f"Expected to see the following lines in the console: \n" + solution +
-                                     "\nBut your output is:\n" + output)
-
-        return CheckResult.correct()
-
-    @dynamic_test()
-    def test_4(self):
-
-        main = TestedProgram()
-        output = main.start("-l", "--save=", "-a")
-
-        solution = "It seems you forget to specify argument!"
+        solution = "The file data.txt exists!"
 
         if len(output) == 0:
             return CheckResult.wrong("It seems that your program does not print anything to the console!")
 
         if len(output.split("\n")[-2]) > len(solution):
             return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
-                                     "\nYour output is:\n" + output + "You may have printed extra parameters!")
+                                     "\nYour output is:\n" + output)
 
         if solution not in output:
             return CheckResult.wrong(f"Expected to see the following lines in the console: \n" + solution +
                                      "\nBut your output is:\n" + output)
 
         return CheckResult.correct()
+
+    @dynamic_test()
+    def test_2(self):
+        main = TestedProgram()
+        output = main.start("--create=audio")
+
+        is_dir = os.path.isdir("audio")
+
+        solution = "The folder audio was created!"
+
+        if len(output) == 0:
+            return CheckResult.wrong("It seems that your program does not print anything to the console!")
+
+        if len(output.split("\n")[-2]) > len(solution):
+            return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
+                                     "\nYour output is:\n" + output)
+
+        if solution not in output:
+            return CheckResult.wrong(f"Expected to see the following lines in the console: \n" + solution +
+                                     "\nBut your output is:\n" + output)
+        if is_dir:
+            return CheckResult.correct()
+
+    @dynamic_test()
+    def test_3(self):
+        main = TestedProgram()
+        output = main.start("--create=videos/first_video")
+
+        is_dir = os.path.isdir("videos/first_video")
+
+        solution = "The folders videos/first_video were created!"
+
+        if len(output) == 0:
+            return CheckResult.wrong("It seems that your program does not print anything to the console!")
+
+        if len(output.split("\n")[-2]) > len(solution):
+            return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
+                                     "\nYour output is:\n" + output)
+
+        if solution not in output:
+            return CheckResult.wrong(f"Expected to see the following lines in the console: \n" + solution +
+                                     "\nBut your output is:\n" + output)
+        if is_dir:
+            return CheckResult.correct()
+
+    @dynamic_test()
+    def test_4(self):
+        main = TestedProgram()
+        output = main.start("--remove=audio")
+
+        is_exist = os.path.exists("audio")
+
+        solution = "The folder audio was deleted!"
+
+        if len(output) == 0:
+            return CheckResult.wrong("It seems that your program does not print anything to the console!")
+
+        if len(output.split("\n")[-2]) > len(solution):
+            return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
+                                     "\nYour output is:\n" + output)
+
+        if solution not in output:
+            return CheckResult.wrong(f"Expected to see the following lines in the console: \n" + solution +
+                                     "\nBut your output is:\n" + output)
+        if not is_exist:
+            return CheckResult.correct()
+
+    @dynamic_test()
+    def test_5(self):
+        main = TestedProgram()
+        output = main.start("--remove=videos/first_video")
+
+        is_exist = os.path.exists("videos/first_video")
+
+        solution = "The folder videos in first_video folder was deleted!"
+
+        if len(output) == 0:
+            return CheckResult.wrong("It seems that your program does not print anything to the console!")
+
+        if len(output.split("\n")[-2]) > len(solution):
+            return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
+                                     "\nYour output is:\n" + output)
+
+        if solution not in output:
+            return CheckResult.wrong(f"Expected to see the following lines in the console: \n" + solution +
+                                     "\nBut your output is:\n" + output)
+        if not is_exist:
+            return CheckResult.correct()
+
+    @dynamic_test()
+    def test_6(self):
+        main = TestedProgram()
+        output = main.start("--remove=fake_folder")
+
+        is_exist = os.path.exists("fake_folder")
+
+        solution = "This fake_folder folder doesn't exist!"
+
+        if len(output) == 0:
+            return CheckResult.wrong("It seems that your program does not print anything to the console!")
+
+        if len(output.split("\n")[-2]) > len(solution):
+            return CheckResult.wrong(f"The output of your program is longer than the expected output: \n" + solution +
+                                     "\nYour output is:\n" + output)
+
+        if solution not in output:
+            return CheckResult.wrong(f"Expected to see the following lines in the console: \n" + solution +
+                                     "\nBut your output is:\n" + output)
+
+        # delete videos folder because it should not exist after tests
+        if os.path.exists("videos"):
+            os.rmdir("videos")
+
+        if not is_exist:
+            return CheckResult.correct()
 
 
 if __name__ == '__main__':
